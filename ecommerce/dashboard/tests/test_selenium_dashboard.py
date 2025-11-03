f7a3048eb22a07246d11a847d894f842d6f87c72
leemutai <mutailee361@gmail.com>
2025-11-03 22:40:00 +0300

    add Selenium test for admin login using live server and Chrome browser

--- ecommerce/dashboard/tests/test_selenium_dashboard.py
+++ ecommerce/dashboard/tests/test_selenium_dashboard.py
@@ -1,7 +1,17 @@
 import pytest
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.common.by import By
 @pytest.mark.selenium
-def test_dashboard_admin_login():
-    pass
+def test_dashboard_admin_login(live_server, chrome_browser_instance):
+    browser = chrome_browser_instance
+
+    browser.get(("%s%s" % (live_server.url, "/admin/login/")))
+
+    user_name = browser.find_element(By.NAME, "username")
+    user_password = browser.find_element(By.NAME, "password")
+    submit = browser.find_element(By.XPATH, '//input[@value="Log in"]')
+
+    user_name.send_keys("admin")
+    user_password.send_keys("admin1234")
+  
     # This is a placeholder for the actual test implementation.
